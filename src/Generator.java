ff9322d5d709d8cb183128bb04f8d769e9e2edbf
Karl Johan Kristensen <Svampen@KJs-MacBook-Pro.local>
2018-04-11 15:49:06 +0200

    Wrote checkEntry function

--- src/Generator.java
+++ src/Generator.java
@@ -1,6 +1,6 @@
-public static class Generator {
+public final class Generator {
     // Returns true if entry is unique in row, column and 3x3-block
-    public static boolean checkEntry(Square entry, Board board){
+    public static boolean checkEntry(Square entry, Square[][] board){
         // Checks if entry is unique in row
         for(int i = 0; i < 9; i++){
             if(board[entry.getRow()][i].getColumn() != entry.getColumn()){
@@ -20,6 +20,14 @@ public static class Generator {
         }
 
         // Checks if entry is unique in 3x3-block
-
+        // Square.getBlock()[0] returns column, while Square.getBlock()[1] returns row
+        for(int i = entry.getBlock()[0]; i < 3; i++){
+            for(int j = entry.getBlock()[1]; i < 3; i++){
+                if(board[i][j].getValue() == entry.getValue()){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
